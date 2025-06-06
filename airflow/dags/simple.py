5737e09a325be9182eff9000ae9cf16d1ac32309
sushilkumart <42314696+sushilkumart@users.noreply.github.com>
2025-06-06 17:46:55 +0530

    Update simple.py

--- airflow/dags/simple.py
+++ airflow/dags/simple.py
@@ -5,7 +5,7 @@ from airflow.utils.dates import days_ago
 def test_task():
     print("Simple task works!")
 
-with DAG("test_dag_simple", start_date=days_ago(1), schedule_interval=None, catchup=False) as dag:
+with DAG("test_dag_simple1", start_date=days_ago(1), schedule_interval=None, catchup=False) as dag:
     task = PythonOperator(
         task_id="simple_task",
         python_callable=test_task
