0c64d20c60aedd15ddea86615619b5baee6cb3b1
sushilkumart <42314696+sushilkumart@users.noreply.github.com>
2025-06-06 18:45:36 +0530

    Update kpo_in_podman.py

--- airflow/dags/kpo_in_podman.py
+++ airflow/dags/kpo_in_podman.py
@@ -1,23 +1,26 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from airflow.utils.dates import days_ago
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+}
 
 with DAG(
-    dag_id="kpo_in_podman",
-    start_date=days_ago(1),
-    schedule_interval=None,
+    dag_id='kubernetes_operator_simple_dag',
+    default_args=default_args,
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
     catchup=False,
+    tags=['example', 'kubernetes'],
 ) as dag:
 
-    k8s_task = KubernetesPodOperator(
-        namespace='default',
-        image="python:3.9-slim",
-        cmds=["python", "-c"],
-        arguments=["print('Hello from K8s pod!')"],
-        labels={"foo": "bar"},
-        name="example-pod",
-        task_id="run_pod_in_k8s",
+    sleep_task = KubernetesPodOperator(
+        task_id="sleep_task",
+        name="sleep-pod",
+        namespace="airflow",  # Ensure this matches your Helm release namespace
+        image="busybox",
+        cmds=["sh", "-c", "echo 'Sleeping for 30s...'; sleep 30; echo 'Done.'"],
         get_logs=True,
         is_delete_operator_pod=True,
-        config_file="/root/.kube/config"
     )
