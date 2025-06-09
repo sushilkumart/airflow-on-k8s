79d597d9ecb36636617fabee8beb0922f9753f8a
sushilkumart <42314696+sushilkumart@users.noreply.github.com>
2025-06-09 12:42:00 +0530

    Update spring_boot_scheduler.py

--- airflow/dags/spring_boot_scheduler.py
+++ airflow/dags/spring_boot_scheduler.py
@@ -2,17 +2,24 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime
 
+default_args = {
+    'owner': 'airflow',
+}
+
 with DAG(
-    dag_id='spring_boot_scheduler',
-    start_date=datetime(2025, 6, 9),
-    schedule_interval=None,
-    catchup=False
+    dag_id='spring_dag',
+    default_args=default_args,
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=['example', 'kubernetes'],
 ) as dag:
-    run_spring_boot = KubernetesPodOperator(
-        task_id='run_spring_boot_task',
-        name='spring-boot-pod',
-        namespace='default',
-        image='sktaskar/airflow1:latest',
-        cmds=["java", "-jar", "app.jar"],
-        get_logs=True
+
+    sleep_task = KubernetesPodOperator(
+        task_id="sleep_task",
+        name="sleep-pod",
+        namespace="airflow",  # Ensure this matches your Helm release namespace
+        image="sktaskar/airflow1",
+        get_logs=True,
+        is_delete_operator_pod=True,
     )
