97b2a0b98d43fe9c62c0dd0dda1548716274831c
sushilkumart <42314696+sushilkumart@users.noreply.github.com>
2025-07-15 10:21:01 +0530

    Add files via upload

--- airflow/dags/spring_boot_scheduler.py
+++ airflow/dags/spring_boot_scheduler.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
@@ -15,11 +15,33 @@ with DAG(
     tags=['example', 'kubernetes'],
 ) as dag:
 
-    sleep_task = KubernetesPodOperator(
-        task_id="sleep_task",
-        name="sleep-pod",
+    airflow_task1 = KubernetesPodOperator(
+        task_id="a1_task",
+        name="a1-pod",
         namespace="airflow",  # Ensure this matches your Helm release namespace
         image="sktaskar/airflow1",
         get_logs=True,
         is_delete_operator_pod=True,
     )
+    
+    airflow_task2 = KubernetesPodOperator(
+      task_id="a2_task",
+      name="a2-pod",
+      namespace="airflow",  # Ensure this matches your Helm release namespace
+      image="sktaskar/airflow1",
+      get_logs=True,
+      execution_timeout=timedelta(minutes=1),
+      is_delete_operator_pod=True,
+    )
+    
+    airflow_task3 = KubernetesPodOperator(
+      task_id="a3_task",
+      name="a3-pod",
+      namespace="airflow",  # Ensure this matches your Helm release namespace
+      image="sktaskar/airflow1",
+      get_logs=True,
+      is_delete_operator_pod=True,
+      trigger_rule="all_success"
+    )
+    
+airflow_task1 >> airflow_task2 >> airflow_task3
